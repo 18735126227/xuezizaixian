41c955827b3fe3fb2ed1dd0c8b27e5f113dda091
hjc <941871365@qq.com>
2025-06-14 11:24:59 +0800

    建立了content微服务的架构

---
+++ xuezi-plus-content/xuezi-plus-content-api/src/main/java/com/xuezi/XueziPlusContentApiApplication.java
@@ -0,0 +1,13 @@
+package com.xuezi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XueziPlusContentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XueziPlusContentApiApplication.class, args);
+    }
+
+}
---
+++ xuezi-plus-content/xuezi-plus-content-service/src/main/java/com/xuezi/XueziPlusContentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xuezi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XueziPlusContentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XueziPlusContentServiceApplication.class, args);
+    }
+
+}
